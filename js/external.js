9ab141931ab63e409ab2c57355cbebfefe173675
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2022-11-21 11:37:39 -0600

    Changed the alert

--- js/external.js
+++ js/external.js
@@ -1,4 +1,4 @@
 console.log("Hello from external JavaScript")
-alert("HELLOOOOo")
+alert("Welcome to my Website!")
 var favColor = prompt("what is your favorite color?")
 alert(" I love" + favColor + " Too!")
